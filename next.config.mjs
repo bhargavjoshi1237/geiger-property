e4c15f9903b8addb8eb2b0e74bcfb26252605629
Jack <bhargavjoshi1237@gmail.com>
2026-07-03 02:15:05 +0530

    updated route

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,7 @@
-const isProd = process.env.NODE_ENV === 'production';
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@geiger/ui"],
-  basePath: isProd ? '/property' : '',
   allowedDevOrigins: ['127.0.0.1'],
-  env: {
-    NEXT_PUBLIC_BASE_PATH: isProd ? '/property' : '',
-  },
 };
 
 export default nextConfig;
